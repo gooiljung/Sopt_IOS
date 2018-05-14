ed26a14bb818401e9bd4ac4a6473da1961ce5985
gooiljung <junggooil9191@gmail.com>
2018-05-14 20:28:09 +0900

    1_2

---
+++ Sopt_Assignment1_2/SoptAssignment1_2/ViewController2.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController2.swift
+//  SoptAssignment1_2
+//
+//  Created by 정보영 on 2018. 4. 5..
+//  Copyright © 2018년 GooIlJung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+
+
+}
---
+++ Sopt_Assignment1_2/SoptAssignment1_2/aa.swift
@@ -0,0 +1,35 @@
+//
+//  aa.swift
+//  SoptAssignment1_2
+//
+//  Created by 정보영 on 2018. 4. 6..
+//  Copyright © 2018년 GooIlJung. All rights reserved.
+//
+
+import UIKit
+
+class aa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
